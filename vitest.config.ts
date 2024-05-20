70ba79544448643429373e13867548ea2e499b50
Russell Oakham <russell.oakham@and.digital>
2024-05-20 10:20:35 +0100

    chore: Update ESLint configuration to include React plugins, parser options, and prettier

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "jsdom",
-    setupFiles: ["./src/tests/setup.ts"],
+    setupFiles: ["./src/tests/setupTests.ts"],
     globals: true,
   },
   plugins: [tsconfigPaths()],
